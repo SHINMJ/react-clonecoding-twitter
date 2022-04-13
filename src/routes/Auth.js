dcca9bbf6624cb819b613fbf1fd3fb596b60e7a4
shinmj <gkdlqkek@gmail.com>
2022-04-13 15:04:26 +0900

    feat: make login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log(email, password)
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          required
+          value={email}
+          onChange={handleChangeEmail}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <input onSubmit={handleSubmit} type='submit' value='Log In' />
+      </form>
+      <button>Continue with Google</button>
+      <button>Continue with Github</button>
+    </div>
+  )
+}
 
 export default Auth
